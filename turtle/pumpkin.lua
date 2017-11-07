a84be800e2d88defa655ec4c914663429d28d1bb
Samuel Beard <samjb2@gmail.com>
2017-11-07 11:39:02 +0000

    Update pumpkin.lua

--- turtle/pumpkin.lua
+++ turtle/pumpkin.lua
@@ -1,7 +1,7 @@
 -- Turtle API v0.2 -- By Samuel Beard
 function u(a)for b=1,a do turtle.up()end end;function f(c)for d=1,c do turtle.forward()end end;function d(e)for f=1,e do turtle.down()end end;function b(g)for h=1,g do turtle.back()end end;function t(i)if i==1 then turtle.turnLeft()end;if i==2 then turtle.turnRight()end;if i==3 then turtle.turnRight()turtle.turnRight()end end;function r(j)turtle.turnRight()f(j)turtle.turnLeft()end;function l(k)turtle.turnLeft()f(k)turtle.turnRight()end
 
-cobbleID = 4
+cobbleID = "minecraft:cobblestone"
 
 function harvestBlock()
   os.setComputerLabel("Harvesting")
@@ -13,7 +13,7 @@ end
 function goBack()
   os.setComputerLabel("Heading Back")
   t(3)
-  while (turtle.detect() == false) then
+  while (turtle.detect() == false) do
     f(1)
   end
   turtle.drop()
@@ -23,7 +23,8 @@ end
 turtle.select(1)
 while true do
   t(3)
-  if (turtle.inspectDown()[2][1] == cobbleID) then
+  local success, data = turtle.inspectDown()
+  if (data.name == cobbleID) then
     goBack()
   else
     harvestBlock()
