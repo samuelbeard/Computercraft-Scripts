8b61d83f874f8b38b5a26eabf2b8da9fc61a98d4
Samuel Beard <sbeard@brightsource.co.uk>
2018-01-11 13:57:50 +0000

    useless

---
+++ turtle/useless/hide.lua
@@ -0,0 +1,9 @@
+-- Turtle API v0.3 -- By Samuel Beard
+function u(a)for b=1,a do turtle.up()end end;function f(c)for d=1,c do turtle.forward()end end;function d(e)for f=1,e do turtle.down()end end;function b(g)for h=1,g do turtle.back()end end;function t(i)if i==1 then turtle.turnLeft()end;if i==2 then turtle.turnRight()end;if i==3 then turtle.turnRight()turtle.turnRight()end end;function r(j)turtle.turnRight()f(j)turtle.turnLeft()end;function l(k)turtle.turnLeft()f(k)turtle.turnRight()end;function floor()while turtle.detectDown()==false do d(1)end end
+
+turtle.select(1)
+turtle.digDown()
+d(1)
+turtle.digDown()
+d(1)
+turtle.placeUp()
