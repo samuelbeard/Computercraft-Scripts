2a97c88ffd42ed6c16a355d544f37805464e686d
vagrant <vagrant@vagrant.vm>
2021-04-30 15:57:03 +0000

    .

--- setup.lua
+++ setup.lua
@@ -2,3 +2,7 @@
 
 shell.run("github get api/move.lua move")
 shell.run("github get api/sort.lua sort")
+
+shell.run("github get turtle/pillar.lua pillar")
+shell.run("github get turtle/walkway.lua walkway")
+shell.run("github get turtle/cane.lua cane")
